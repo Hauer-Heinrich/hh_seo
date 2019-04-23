d63b8a545e188adc46b19cd42ab96c1e4d696875
Christian Hackl <hackl.chris@googlemail.com>
2019-04-23 12:56:41 +0200

    added posibility to use viewhelper arguments

--- Classes/ViewHelpers/MetaTagViewHelper.php
+++ Classes/ViewHelpers/MetaTagViewHelper.php
@@ -39,7 +39,39 @@ class MetaTagViewHelper extends AbstractViewHelper {
 
     public function initializeArguments() {
         $this->registerArgument('order', 'int', 'Ordering int', true);
-        $this->registerArgument('data', 'array', 'Array', false);
+
+        $this->registerArgument('title', 'string', 'Title-Tag');
+        $this->registerArgument('titleBefore', 'string', 'Title before string');
+        $this->registerArgument('titleAfter', 'string', 'Title after string');
+        $this->registerArgument('titleSeparate', 'string', 'Title seperator');
+        $this->registerArgument('titleSeparateBefore', 'string', 'Title seperator before (overrides titleSeparate)');
+        $this->registerArgument('titleSeparateAfter', 'string', 'Title seperator after (overrides titleSeparate)');
+        $this->registerArgument('description', 'string', 'Description-Tag');
+
+        $this->registerArgument('designer', 'string', 'Designer');
+        $this->registerArgument('theme-color', 'string', 'theme-color');
+        $this->registerArgument('touchIcon', 'string', 'touch icon - output for devers gadgets, shouldbe 310x310px');
+        $this->registerArgument('imagetoolbar', 'boolean', 'Imagetoolbar - boolean');
+        $this->registerArgument('format-detection', 'boolean', 'Autoformat phonenumbers - on various gadgets');
+        $this->registerArgument('last-modified', 'int', 'last-modified as timestamp');
+        $this->registerArgument('author', 'string', 'Author');
+        $this->registerArgument('copyright', 'string', 'Copyright´');
+
+        $this->registerArgument('robots-index', 'string', 'robots index');
+        $this->registerArgument('robots-follow', 'string', 'robots follow');
+
+        $this->registerArgument('og-title', 'string', 'OpenGraph title e. g. for facebook');
+        $this->registerArgument('og-description', 'string', 'OpenGraph description e. g. for facebook');
+        $this->registerArgument('og-image', 'string', 'OpenGraph image absolute path e. g. for facebook');
+
+        $this->registerArgument('twitter-title', 'string', 'Twitter title');
+        $this->registerArgument('twitter-description', 'string', 'Twitter description');
+        $this->registerArgument('twitter-image', 'string', 'Twitter image absolute path');
+
+        $this->registerArgument('geo-region', 'string', 'Countrycode - regioncode e. g. DE-BY for Germany-Bavaria');
+        $this->registerArgument('geo-placename', 'string', 'City name');
+        $this->registerArgument('geo-position:long', 'double', 'longitude');
+        $this->registerArgument('geo-position:lat', 'double', 'latitude');
         $this->registerArgument('override', 'boolean', 'Overwrites the data with lower order completely', false);
     }
 
@@ -51,13 +83,19 @@ class MetaTagViewHelper extends AbstractViewHelper {
      * @return string
      */
     public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext) {
+        $dataArray = [
+            'headerData' => $arguments
+        ];
+        $newHeaderData = [];
+        $headerData = [];
+
         $renderChildren = $renderChildrenClosure();
         if(!empty(trim($renderChildren))) {
             $newHeaderData = json_decode($renderChildren, true);
-            $newHeaderData['override'] = $arguments['override'];
-            $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['hh_seo']['MetaTag'][$arguments['order']] = $newHeaderData;
-        } else if(isset($arguments['data'])) {
-            $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['hh_seo']['MetaTag'][$arguments['order']] = $arguments['data'];
+            $headerData = $newHeaderData[0];
+            $headerData['override'] = $arguments['override'];
         }
+
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['hh_seo']['MetaTag'][$arguments['order']] = array_replace_recursive($dataArray, $headerData);
     }
 }
