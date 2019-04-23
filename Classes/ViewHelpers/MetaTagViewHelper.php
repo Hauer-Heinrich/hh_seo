2ffc5afa65f22a48437e19b2ac6506c0683c6532
ioCron <info@iocron.com>
2019-04-23 19:58:59 +0200

    Added registerArguments() Method to MetaTagViewHelper

--- Classes/ViewHelpers/MetaTagViewHelper.php
+++ Classes/ViewHelpers/MetaTagViewHelper.php
@@ -30,7 +30,7 @@ namespace HauerHeinrich\HhSeo\ViewHelpers;
  *  EXAMPLE: Resources/Private/Templates/Example.html
  */
 
-// use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use function GuzzleHttp\json_decode;
@@ -38,41 +38,48 @@ use function GuzzleHttp\json_decode;
 class MetaTagViewHelper extends AbstractViewHelper {
 
     public function initializeArguments() {
-        $this->registerArgument('order', 'int', 'Ordering int', true);
+        $this->registerArguments([
+            ['order', 'int', 'Ordering int', true],
+            ['title', 'string', 'Title-Tag'],
+            ['titleBefore', 'string', 'Title before string'],
+            ['titleAfter', 'string', 'Title after string'],
+            ['titleSeparate', 'string', 'Title seperator'],
+            ['titleSeparateBefore', 'string', 'Title seperator before (overrides titleSeparate)'],
+            ['titleSeparateAfter', 'string', 'Title seperator after (overrides titleSeparate)'],
+            ['description', 'string', 'Description-Tag'],
 
-        $this->registerArgument('title', 'string', 'Title-Tag');
-        $this->registerArgument('titleBefore', 'string', 'Title before string');
-        $this->registerArgument('titleAfter', 'string', 'Title after string');
-        $this->registerArgument('titleSeparate', 'string', 'Title seperator');
-        $this->registerArgument('titleSeparateBefore', 'string', 'Title seperator before (overrides titleSeparate)');
-        $this->registerArgument('titleSeparateAfter', 'string', 'Title seperator after (overrides titleSeparate)');
-        $this->registerArgument('description', 'string', 'Description-Tag');
+            ['designer', 'string', 'Designer'],
+            ['theme-color', 'string', 'theme-color'],
+            ['touchIcon', 'string', 'touch icon - output for devers gadgets, shouldbe 310x310px'],
+            ['imagetoolbar', 'boolean', 'Imagetoolbar - boolean'],
+            ['format-detection', 'boolean', 'Autoformat phonenumbers - on various gadgets'],
+            ['last-modified', 'int', 'last-modified as timestamp'],
+            ['author', 'string', 'Author'],
+            ['copyright', 'string', 'Copyright'],
 
-        $this->registerArgument('designer', 'string', 'Designer');
-        $this->registerArgument('theme-color', 'string', 'theme-color');
-        $this->registerArgument('touchIcon', 'string', 'touch icon - output for devers gadgets, shouldbe 310x310px');
-        $this->registerArgument('imagetoolbar', 'boolean', 'Imagetoolbar - boolean');
-        $this->registerArgument('format-detection', 'boolean', 'Autoformat phonenumbers - on various gadgets');
-        $this->registerArgument('last-modified', 'int', 'last-modified as timestamp');
-        $this->registerArgument('author', 'string', 'Author');
-        $this->registerArgument('copyright', 'string', 'Copyright´');
+            ['robots-index', 'string', 'robots index'],
+            ['robots-follow', 'string', 'robots follow'],
 
-        $this->registerArgument('robots-index', 'string', 'robots index');
-        $this->registerArgument('robots-follow', 'string', 'robots follow');
+            ['og-title', 'string', 'OpenGraph title e. g. for facebook'],
+            ['og-description', 'string', 'OpenGraph description e. g. for facebook'],
+            ['og-image', 'string', 'OpenGraph image absolute path e. g. for facebook'],
 
-        $this->registerArgument('og-title', 'string', 'OpenGraph title e. g. for facebook');
-        $this->registerArgument('og-description', 'string', 'OpenGraph description e. g. for facebook');
-        $this->registerArgument('og-image', 'string', 'OpenGraph image absolute path e. g. for facebook');
+            ['twitter-title', 'string', 'Twitter title'],
+            ['twitter-description', 'string', 'Twitter description'],
+            ['twitter-image', 'string', 'Twitter image absolute path'],
 
-        $this->registerArgument('twitter-title', 'string', 'Twitter title');
-        $this->registerArgument('twitter-description', 'string', 'Twitter description');
-        $this->registerArgument('twitter-image', 'string', 'Twitter image absolute path');
+            ['geo-region', 'string', 'Countrycode - regioncode e. g. DE-BY for Germany-Bavaria'],
+            ['geo-placename', 'string', 'City name'],
+            ['geo-position:long', 'double', 'longitude'],
+            ['geo-position:lat', 'double', 'latitude'],
+            ['override', 'boolean', 'Overwrites the data with lower order completely', false]
+        ]);
+    }
 
-        $this->registerArgument('geo-region', 'string', 'Countrycode - regioncode e. g. DE-BY for Germany-Bavaria');
-        $this->registerArgument('geo-placename', 'string', 'City name');
-        $this->registerArgument('geo-position:long', 'double', 'longitude');
-        $this->registerArgument('geo-position:lat', 'double', 'latitude');
-        $this->registerArgument('override', 'boolean', 'Overwrites the data with lower order completely', false);
+    function registerArguments(Array $registers){
+        foreach($registers as $registerKey => $registerVal){
+            $this->registerArgument(...$registerVal);
+        }
     }
 
     /**
