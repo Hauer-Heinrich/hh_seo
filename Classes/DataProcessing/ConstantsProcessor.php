81a3e254d4b42dbbf6f60c5b6427e488b8203a6d
Christian Hackl <hackl.chris@googlemail.com>
2025-01-28 16:10:19 +0100

    [BUGFIX] missing constants for fluid

--- Classes/DataProcessing/ConstantsProcessor.php
+++ Classes/DataProcessing/ConstantsProcessor.php
@@ -11,7 +11,12 @@ class ConstantsProcessor implements DataProcessorInterface {
      * process
      */
     public function process(ContentObjectRenderer $cObj, array $contentObjectConfiguration, array $processorConfiguration, array $processedData): array {
-        $settings = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_hhseo.'];
+        $settings = [];
+        if(isset($GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_hhseo.'])) {
+            $settings = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_hhseo.'];
+        } else {
+            $settings = isset($contentObjectConfiguration['settings.']) ? $contentObjectConfiguration['settings.'] : [];
+        }
         $targetVariableName = $cObj->stdWrapValue('as', $processorConfiguration, 'hhSeo');
         $processedData[$targetVariableName] = $settings;
 
